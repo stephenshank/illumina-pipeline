196bfbb313eba77ecf7f1cdf0821028b7444b907
Stephen Shank <sshank314@gmail.com>
2025-02-25 21:36:28 -0500

    Updated reference, add segments to intrahost visualization.

--- mlip/visualization.py
+++ mlip/visualization.py
@@ -8,30 +8,39 @@ import altair as alt
     
 
 def replicate_variant_plot(input_tsv_filepath, output_html_filepath):
-        df = pd.read_csv(input_tsv_filepath, sep='\t')
-        chart = alt.Chart(df, width=400, height=400).mark_point().encode(
-            x=alt.X('Frequency_1:Q', title='Sample 1 variant frequency'),
-            y=alt.Y('Frequency_2:Q', title='Sample 2 variant frequency'),
-            tooltip=[
-                alt.Tooltip('segment:N', title='Segment'),
-                alt.Tooltip('position:Q', title='Position'),
-                alt.Tooltip('alt:N', title='Alternative'),
-                alt.Tooltip('Frequency_1:Q', title='Frequency 1'),
-                alt.Tooltip('Frequency_2:Q', title='Frequency 2')
-            ]
-        ).interactive().properties(
-            title=alt.TitleParams(
-                text='Replicate variant calls',
-                fontSize=20,
-                anchor='middle'
-            ),
-            padding={'left': 20, 'right': 20, 'top': 10, 'bottom': 10}
-        ).configure_axis(
-            labelFontSize=18,
-            titleFontSize=18
-        )
-        chart.save(output_html_filepath)
-
+    df = pd.read_csv(input_tsv_filepath, sep='\t')
+    chart = alt.Chart(df, width=400, height=400).mark_point(
+        size=100, filled=True, opacity=.6
+    ).encode(
+        x=alt.X('Frequency_1:Q',
+                title='Sample 1 variant frequency',
+                scale=alt.Scale(domain=[0, 0.5])),
+        y=alt.Y('Frequency_2:Q',
+                title='Sample 2 variant frequency',
+                scale=alt.Scale(domain=[0, 0.5])),
+        color=alt.Color('segment:N', title='Segment'),
+        tooltip=[
+            alt.Tooltip('segment:N', title='Segment'),
+            alt.Tooltip('position:Q', title='Position'),
+            alt.Tooltip('alt:N', title='Alternative'),
+            alt.Tooltip('Frequency_1:Q', title='Frequency 1'),
+            alt.Tooltip('Frequency_2:Q', title='Frequency 2')
+        ]
+    ).interactive().properties(
+        title=alt.TitleParams(
+            text='Replicate variant calls',
+            fontSize=24,
+            anchor='middle'
+        ),
+        padding={'left': 20, 'right': 20, 'top': 10, 'bottom': 10}
+    ).configure_axis(
+        labelFontSize=20,
+        titleFontSize=20
+    ).configure_legend(
+        titleFontSize=20,
+        labelFontSize=20
+    )
+    chart.save(output_html_filepath)
 
 
 def find_free_port():
